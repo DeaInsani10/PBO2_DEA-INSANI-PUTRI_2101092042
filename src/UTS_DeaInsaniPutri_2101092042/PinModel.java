5f79847bd090afe3e771ad48653e9e81fec0a794
DeaInsani10 <deainsani1001@gmail.com>
2023-05-31 12:29:32 +0700

    Project 26 April 2023

---
+++ src/UTS_DeaInsaniPutri_2101092042/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS_DeaInsaniPutri_2101092042;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PinModel {
+   
+    String pin = "100103";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
